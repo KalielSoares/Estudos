fa4f3c751a0ff9557f8380a248b89af09877234e
Kaliel <Kalielsoares6@gmail.com>
2023-08-02 22:31:53 -0300

    Feat: Initial Commit

--- Javascript Modulo 4/ExercicioJs - 19/Post.js
+++ Javascript Modulo 4/ExercicioJs - 19/Post.js
@@ -1,6 +1,18 @@
+const Comment = require("./Comment")
+
+
 class Post{
-    constructor(text,date){
+    constructor(text,body,author){
         this.text = text,
-        this.date = date
+        this.body = body,
+        this.author = author,
+        this.comments = [],
+        this.createAt = new Date()
+    }
+
+    addComment(username,content){
+        this.comments.push(new Comment(username, content))
     }
 }
+
+module.exports = Post
