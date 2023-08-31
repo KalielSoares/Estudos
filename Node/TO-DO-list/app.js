2da1935d265384aabf15fc9d1c450b40707ae0ac
Kaliel <Kalielsoares6@gmail.com>
2023-08-31 12:23:19 -0300

    Final commit

--- Node/TO-DO-list/app.js
+++ Node/TO-DO-list/app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const path = require('path');
-
+const methodOverride = require('method-override');
 const checkListRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
-const methodOverride = require('method-override');
+const taskRouter = require('./src/routes/tasks');
 
 require('./config/database');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'));
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method', { methods: ['POST', 'GET']}));
 
 app.set('views', path.join(__dirname, 'src/views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use('/', rootRouter);
-app.use('/checklists', checkListRouter);
+app.use('/checklist', checkListRouter);
+app.use('/checklist', taskRouter.checklistDependent);
 
 
-app.listen(3000,() => {
-    console.log('Servidor Ativo')
+app.listen(3000, ()=>{
+  console.log('Server iniciado');
 })
--- Node/TO-DO-list/src/routes/checklist.js
+++ Node/TO-DO-list/src/routes/checklist.js
@@ -1,87 +1,92 @@
-const express = require ('express');
-const Checklist = require('../models/checklist');
+const express = require('express');
+const checklist = require('../models/checklist');
 
 const router = express.Router();
 
+const Checklist = require('../models/checklist');
 
-
-router.get('/', async (req,res) => {
-    try {
-        let checklists = await Checklist.find({});
-        res.status(200).render('checklists/index', {checklists: checklists});
-    } catch (error) {
-        res.status(422).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'})
-    }
-})
+router.get('/', async (req, res) => {
+  try {
+    let checklist = await Checklist.find({});
+    res.status(200).render('checklists/index', { checklists: checklist })
+  } catch (error) {
+    res.status(422).json(error);
+    res.status(422).render('pages/error', {error: "Erro ao exibir listas!"});
+  }
+});
 
 router.get('/new', async (req, res) => {
-    try {
-        let checklist = new Checklist();
-        res.status(200).render('checklists/new', { checklist: checklist})
-    } catch (error) {
-        res.status(500).render('pages/error', { errors: 'Erro ao carregar o formulário'})
-    }
-})
+  try{
+    let checklist = new Checklist();
+    res.status(200).render('checklists/new', { checklist: checklist })
+  }catch(error){
+    res.status(422).json(error);
+    console.log("error", error)
+    res.status(422).render('pages/error', {error: "Erro ao criar tarefa!"});
+  }
+});
 
 router.get('/:id/edit', async (req, res) => {
-    try{
-      let checklist = await Checklist.findById(req.params.id);
-      res.status(200).render('checklists/edit', { checklist: checklist })
-    }catch(error){
-      res.status(422).json(error);
-      console.log("error", error)
-      res.status(422).render('pages/error', {error: "Erro ao exibir página de edição!"});
-    }
-  });
+  try{
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render('checklists/edit', { checklist: checklist })
+  }catch(error){
+    res.status(422).json(error);
+    console.log("error", error)
+    res.status(422).render('pages/error', {error: "Erro ao exibir página de edição!"});
+  }
+});
 
 router.post('/', async (req, res) => {
-    let { name } = req.body.checklist;
-    let checklist = new Checklist({name})
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({name})
+  
+  try{
+    console.log('CHECK',req.body)
+    await checklist.save();
+    res.redirect('/checklist');
+  }catch(error){
+    res.status(422).render('checklists/new', {checklist: {...checklist, error}});
+    console.log("error", error)
+  }
 
-    try {
-        await checklist.save()
-        res.redirect('/checklists')
-    } catch (error) {
-        res.status(422).render('checklists/new', { checklist: {...checklist, error}})
-    }
-    
-    console.log(name);
-    
-})
+});
 
-router.get('/:id', async (req,res) => {
-    try {
-        let checklist = await Checklist.findById(req.params.id);
-        res.status(200).render('checklists/show', {checklist: checklist});
-    } catch (error) {
-        res.status(500).render('pages/error', {error: 'Erro ao exibir as Listas de Tarefas'})
-    }
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    //A gente quer que devolva as tasks associadas com essa checklist, por isso passamos o populate. Logo o mongoose vai buscar essas tasks e trazer para popular esse array de tasks
+    let checklist = await Checklist.findById(req.params.id).populate('tasks'); 
+    res.status(200).render('checklists/show', { checklists: checklist })
+  } catch (error) {
+    console.log("error", error)
+    res.status(422).json(error);
+    res.status(422).render('pages/error', {error: "Erro ao exibir listas!"});
+  }
 })
 
 router.put('/:id', async (req, res) => {
-    let { name } = req.body.checklist;
-    let checklist = await Checklist.findById(req.params.id);
-    try {
-      await checklist.update({name})
-      res.redirect('/checklist');
-    } catch (error) {
-      console.log("error", error)
-      let errors = error.errors;
-      res.status(422).render('checklists/edit', {checklist: {...checklist, errors}})
-      res.status(422).json(error);
-    }
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
+  try {
+    await checklist.update({name})
+    res.redirect('/checklist');
+  } catch (error) {
+    console.log("error", error)
+    let errors = error.errors;
+    req.status(422).render('checklists/edit', {checklist: {...checklist, errors}})
+    res.status(422).json(error);
+  }
 })
 
-
-router.delete('/:id', async (req,res) => {
-    try {
-        let { name } = req.body
-        let checklist = await Checklist.findByIdAndRemove(req.params.id);
-        res.status(200).json(checklist);
-    } catch (error) {
-        res.status(422).json(error)
-    }
+router.delete('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    res.redirect('/checklist');
+  } catch (error) {
+    res.status(422).render('pages/error', {error: "Erro ao deletar lista de tarefas!"});
+  }
 })
-
 
 module.exports = router;
